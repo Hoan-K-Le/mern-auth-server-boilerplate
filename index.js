184bb0284146b64fd39e5ee0fb00a74a4a251a7c
Hoan Le <hoanleecr@gmail.com>
2022-06-22 11:06:47 -0700

    schema for user

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const cors = require('cors')
 // app config/middlewares
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.use(cors())
 app.use(express.json()) // json req.bodies
 
